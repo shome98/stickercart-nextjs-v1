aeed21efa267f1b5b6e2a879218f132558326817
Sowparna Shome <soupornoshome@gmail.com>
2024-11-19 00:13:04 +0530

    login user route modify

--- app/api/users/login-user/route.ts
+++ app/api/users/login-user/route.ts
@@ -1,32 +1,54 @@
+import dbConnect from "@/db/dbConnection";
 import { User } from "@/db/models/user.model";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function POST(req:Request){
-    const {email,password}=await req.json();
-    if(!email && !password) {
-        return NextResponse.json(
-            { success: false, error: "Both email and password are required." },
-            { status: 400 }
-          );
-    }
-    const existingUser = await User.findOne({ email });
-        if (!existingUser) {
+export async function POST(req: Request) {
+  try {
+    await dbConnect();
+
+    const { email, password } = await req.json();
+    if (!email || !password) {
       return NextResponse.json(
-        { success: false, error: "Looks like you don't have an account here. Please Sign up first." },
-        { status: 409 }
+        { success: false, error: "Both email and password are required." },
+        { status: 400 }
       );
     }
-    if(existingUser.password!==password){
-        return NextResponse.json(
-            { success: false, error: "Looks like you have entered a wrong password." },
-            { status: 409 }
-          );
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      return NextResponse.json(
+        { success: false, error: "Account does not exist. Please sign up." },
+        { status: 404 }
+      );
     }
-    const cookie=cookies();
-    (await cookie).set("login","user is logged in");
+
+    const isPasswordValid = await existingUser.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+      return NextResponse.json(
+        { success: false, error: "Invalid password. Please try again." },
+        { status: 401 }
+      );
+    }
+
+    const accessToken = existingUser.generateAccessToken();
+    const refreshToken = existingUser.generateRefreshToken();
+
+    existingUser.refreshToken = refreshToken;
+    await existingUser.save();
+
+    const cookie = cookies();
+    (await cookie).set("accessToken", accessToken, { httpOnly: true, maxAge: 15 * 60,secure:true }); 
+    (await cookie).set("refreshToken", refreshToken, { httpOnly: true, maxAge: 7 * 24 * 60 * 60,secure:true });
+
     return NextResponse.json(
-        {success:true,message:"you have successfully logged in."},
-        {status:201}
+      { success: true, message: "Successfully logged in." },
+      { status: 200 }
     );
+  } catch (error: unknown) {
+    console.error(error);
+
+    const errorMessage = error instanceof Error ? error.message : "Internal Server Error";
+    return NextResponse.json({ success: false, error: errorMessage }, { status: 500 });
+  }
 }
