712a98866234f5cd4c1f09b693f736780838af0a
shome98 <soupornoshome@gmail.com>
2024-11-19 19:15:41 +0530

    rebounce already connected

--- db/dbConnection.ts
+++ db/dbConnection.ts
@@ -1,8 +1,18 @@
 "use server"
 import mongoose from 'mongoose';
+type ConnectionObject = {
+    isConnected?: number;
+  };
+  
+  const connection: ConnectionObject = {};
 const dbConnect = async () => {
+    if (connection.isConnected) {
+        console.log('Already connected to the database');
+        return;
+      }
     try {
-        await mongoose.connect(process.env.MONGODB_URI as string);
+        const db=await mongoose.connect(process.env.MONGODB_URI as string|| '', {});
+        connection.isConnected = db.connections[0].readyState;
         console.log('MongoDB connected!!!');
     } catch (error) {
         console.error('MongoDB connection failed!!! ', error);
