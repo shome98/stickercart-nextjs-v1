b5c6c1661fcf17429ba8d70100ca4aba7213226d
shome98 <soupornoshome@gmail.com>
2024-11-20 16:11:39 +0530

    product model

---
+++ db/models/product.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IProduct{
+    title:string;
+    description:string;
+    category:string|Schema.Types.ObjectId;
+    price:number;
+    image:{url:string};
+    subImages:[{url:string}];
+    createdBy:string|Schema.Types.ObjectId;
+    inventory:number;
+}
+const productSchema=new Schema<IProduct>({
+    title:{type:String,required:true},
+    description:{type:String,required:true},
+    price:{type:Number,default:0},
+    image:{type:{url:String}},
+    subImages:{type:[{url:String}]},
+    inventory:{type:Number,default:0},
+    category:{type:Schema.Types.ObjectId,ref:"Category"},
+    createdBy:{type:Schema.Types.ObjectId,ref:"User"}
+},{timestamps:true});
+export const Product=mongoose.model<IProduct>("Product",productSchema);
