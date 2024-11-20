824ec5bf757984850948d40fd98780f972291ccf
shome98 <soupornoshome@gmail.com>
2024-11-20 16:10:46 +0530

    category model

---
+++ db/models/category.model.ts
@@ -0,0 +1,11 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ICategory extends Document{
+    title:string;
+    createdBy:string|Schema.Types.ObjectId;
+}
+const categorySchema=new Schema<ICategory>({
+    title:{type:String, required:true},
+    createdBy:{type:Schema.Types.ObjectId,ref:"User"}
+})
+export const Category=mongoose.model<ICategory>("Category",categorySchema);
